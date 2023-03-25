98698c37f91fe9d9794aab2146cf615630ecf1cd
Etheryen <lukasz.ciwoniuk@gmail.com>
2023-03-25 23:11:33 +0100

    working leaderboard with db

--- js/index.js
+++ js/index.js
@@ -7,12 +7,49 @@ async function updateLeaderboard() {
     password: process.env.DATABASE_PASSWORD,
   };
 
+  const punktacja = {
+    1: 3,
+    2: 2,
+    3: 1,
+    4: 0,
+    5: -1,
+    6: -2,
+  };
+
   const conn = connect(config);
-  const MiejscaKoncowe = await conn.execute('SELECT * FROM MiejscaKoncowe');
-  const Gospodynie = await conn.execute('SELECT * FROM Gospodynie');
-  console.log(MiejscaKoncowe);
-  console.log(Gospodynie);
-  //const table = document.querySelector('#board');
+  const query = `
+    SELECT Miejsce, Imie
+    FROM MiejscaKoncowe, Gospodynie
+    WHERE MiejscaKoncowe.IdGospodyni = Gospodynie.IdGospodyni
+  `;
+  const result = await conn.execute(query);
+
+  const punktyGospodyn = new Map();
+
+  for (const row of result.rows) {
+    const currentPoints = punktyGospodyn.get(row.Imie) ?? 0;
+    punktyGospodyn.set(row.Imie, currentPoints + punktacja[row.Miejsce]);
+  }
+
+  const punktySorted = new Map(
+    [...punktyGospodyn.entries()].sort((a, b) => b[1] - a[1])
+  );
+
+  const table = document.querySelector('#board');
+  table.innerHTML = '';
+
+  for (const [imie, punkty] of punktySorted) {
+    const tableRow = document.createElement('tr');
+    const imieEl = document.createElement('td');
+    const punktyEl = document.createElement('td');
+
+    imieEl.innerHTML = imie;
+    punktyEl.innerHTML = punkty;
+
+    tableRow.appendChild(imieEl);
+    tableRow.appendChild(punktyEl);
+    table.appendChild(tableRow);
+  }
 }
 
 updateLeaderboard();
