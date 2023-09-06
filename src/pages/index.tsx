791530d60a56821f337de7115184e27fbe6c0119
Etheryen <lukasz.ciwoniuk@gmail.com>
2023-09-06 19:35:29 +0200

    code cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import Layout from "~/components/layout";
-
 import { api } from "~/utils/api";
 
 export default function Home() {
