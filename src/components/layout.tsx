0d03dc383867c31603fac57e8481b9af4fab91c0
Etheryen <lukasz.ciwoniuk@gmail.com>
2023-09-28 09:29:39 +0200

    improve browser support

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -38,7 +38,7 @@ export default function Layout({
       </Head>
       <main
         data-theme={theme}
-        className="flex min-h-[100dvh] flex-col gap-6 p-6"
+        className="flex min-h-screen flex-col gap-6 p-6 supports-[height:100dvh]:min-h-[100dvh]"
       >
         <Navbar />
         <div
